f63d9742a368bd4967459d06fb31e6eb2e237491
Mihir Kalariya <124235902+KalariyaMihir@users.noreply.github.com>
2023-06-05 00:23:20 +0530

    Add files via upload

---
+++ Task/Positive_negative.java
@@ -0,0 +1,21 @@
+// Write a Program to check the given number is Positive, Negative.
+public class Positive_negative 
+{
+	public static void main(String[] args)
+	{
+		int a = 32;
+		
+		// if else for check the condition 
+		if (a >= 0)
+		{
+			System.out.println("Given Number is Positive Number. ");
+	
+		}
+		
+		else
+		{
+			System.out.println("Given Number is Negative. ");
+		}
+		
+	}
+}
