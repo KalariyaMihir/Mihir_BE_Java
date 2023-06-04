e7ecbd124a09dd8cc34491cb988e4aec4c69c74c
Mihir Kalariya <124235902+KalariyaMihir@users.noreply.github.com>
2023-06-04 14:16:49 +0530

    Add files via upload

---
+++ Task/Name.java
@@ -0,0 +1,12 @@
+
+public class Name 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("My Name is Mihir Kalariya");
+		System.out.println("My Birthdate is 03/05/2001");
+		System.out.println("I am 22 years old ");
+		System.out.println("My City is Rajkot");
+	}
+	
+}
